f15e563ce87fc9144203aeade1dfced8064d8f60
Seonguk. Lim <dnrdl6817@gmail.com>
2023-01-02 17:42:35 +0900

    20230102

---
+++ 샘플데이터베이스(sakila)/board.sql
@@ -0,0 +1,4 @@
+insert into board (title, contents, userid , category)
+ values("제목", "내용" , "작성자", "게시글 분류");
+
+SELECT * FROM board;
---
+++ 샘플데이터베이스(sakila)/books.sql
@@ -0,0 +1,4 @@
+insert into books (title, price, writer, descript)
+ values("빠랍", 9000 , "가와", "누구나 따라하는 JSON");
+
+SELECT * FROM books;	
